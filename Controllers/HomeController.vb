ec5103be8cb425fcd531aad3628eea68c8ce504d
lello <platform@uniscientia.it>
2021-05-21 16:36:07 +0200

    Revert "commit secondo"
    This reverts commit ea02aac889df65ea13f4e1a771609b11ab418cc2.

--- Controllers/HomeController.vb
+++ Controllers/HomeController.vb
@@ -12,7 +12,7 @@
     End Function
 
     Function Contact() As ActionResult
-        ViewData("Message") = "Your pisellino."
+        ViewData("Message") = "Your contact page."
 
         Return View()
     End Function
